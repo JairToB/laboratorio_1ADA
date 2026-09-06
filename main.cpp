cfcff7afe72108ade89c2e8e7f9adc587055f9b9
JairToB <jtorresba@unsa.edu.pe>
2026-09-05 23:30:53 -0500

    Medicion automatica de tiempos duplicando n de 1000 a 8000

--- main.cpp
+++ main.cpp
@@ -61,29 +61,28 @@ double measureTime(int (*algo)(int*, int), int* a, int n, int& result) {
 }
 
 int main(){
-    int n = 8000;
-    int a[n];
+    int a[8000];
     int min = -1000;
     int max = 1000;
-    for (int j = 0; j < n; ++j){
-        int x = generateRandomNumbers(min, max);
-        a[j] = x;
-        std::cout << a[j] << std::endl;
+    for(int i = 1000; i <= 8000; i *= 2){
+        for (int j = 0; j < i; ++j){
+            int x = generateRandomNumbers(min, max);
+            a[j] = x;
+        }
+        int sumMax_firstW;
+        int sumMax_secondW;
+        int sumMax_thirdW;
+
+        double timeCubic = measureTime(cubicMaxSub, a, i, sumMax_firstW);
+        double timeQuadratic = measureTime(quadraticMaxSub, a, i, sumMax_secondW);
+        double timeKadane = measureTime(kadane, a, i, sumMax_thirdW);
+
+        std::cout << "Suma Cubica Maxima: " << sumMax_firstW << " Tiempo Cubico = " << timeCubic << " us\n";
+        std::cout << "Suma Cuadratica Maxima: " << sumMax_secondW << " Tiempo Cuadratico = " << timeQuadratic << " us\n";
+        std::cout << "Suma Kadane Maxima: " << sumMax_thirdW << " Tiempo Kadane = " << timeKadane << " us\n";
+        std::cout << std::endl;
+
     }
-    int sumMax_firstW = cubicMaxSub(a, n);
-    int sumMax_secondW = quadraticMaxSub(a, n);
-    int sumMax_thirdW = kadane(a, n);
-    std::cout << "Suma Cubica Maxima: " << sumMax_firstW << std::endl;
-    std::cout << "Suma Cuadratica Maxima: " << sumMax_secondW << std::endl;
-    std::cout << "Suma Kadane Maxima: " << sumMax_thirdW << std::endl;
-
-    double timeCubic = measureTime(cubicMaxSub, a, n, sumMax_firstW);
-    double timeQuadratic = measureTime(quadraticMaxSub, a, n, sumMax_secondW);
-    double timeKadane = measureTime(kadane, a, n, sumMax_thirdW);
-
-    std::cout << "Tiempo Cubico = " << timeCubic << " us\n";
-    std::cout << "Tiempo Cuadratico = " << timeQuadratic << " us\n";
-    std::cout << "Tiempo Kadane = " << timeKadane << " us\n";
-    std::cout << std::endl;
-    return 0;
+        return 0;
 }
+
